62f5495514841ff627ee7d5a9ae67f3894394abf
FredNekrasov <frednekrasov49@gmail.com>
2025-01-17 19:34:49 +0300

    creating 2 type of aidl services, one of them with callbacks

---
+++ aidlservice/src/main/java/com/fredprojects/aidlservice/UserServiceWithCallbacks.kt
@@ -0,0 +1,49 @@
+package com.fredprojects.aidlservice
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.fredprojects.aidlsdk.UserAIDLWithCallback
+import com.fredprojects.aidlsdk.models.UserInfo
+import com.fredprojects.aidlsdk.utils.ResultCallback
+import kotlinx.coroutines.*
+
+class UserServiceWithCallbacks : Service() {
+    private val scope by lazy { CoroutineScope(Dispatchers.IO) }
+    private var userData: UserInfo? = null
+    override fun onBind(intent: Intent): IBinder {
+        return object : UserAIDLWithCallback.Stub() {
+            override fun getUserInfo(callback: ResultCallback?) {
+                if(callback == null) {
+                    Log.e("fred", "callback is null")
+                    return
+                }
+                try {
+                    scope.launch {
+                        delay(3000)
+                        callback.onSuccess(userData)
+                    }
+                } catch (e: InterruptedException) {
+                    callback.onError(e.message)
+                }
+            }
+
+            override fun setUserInfo(userInfo: UserInfo?, callback: ResultCallback?) {
+                if(callback == null) {
+                    Log.e("fred", "callback is null")
+                    return
+                }
+                try {
+                    scope.launch {
+                        delay(3000)
+                        userData = userInfo
+                        callback.onSuccess(userInfo)
+                    }
+                } catch(e: InterruptedException) {
+                    callback.onError(e.message)
+                }
+            }
+        }
+    }
+}
