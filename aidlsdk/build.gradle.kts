7998dab2d4d4dfa8b00817308210306b1c3042a7
FredNekrasov <frednekrasov49@gmail.com>
2025-01-18 13:28:08 +0300

    adding the kotlin to the aidlsdk module

--- aidlsdk/build.gradle.kts
+++ aidlsdk/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.library)
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -29,9 +30,13 @@ android {
     buildFeatures {
         aidl = true
     }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
 }
 
 dependencies {
+    implementation(libs.androidx.core.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
