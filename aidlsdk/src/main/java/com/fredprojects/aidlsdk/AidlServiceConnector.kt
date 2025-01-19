a9454077ec8590a32b5c86fc45f8e96fdfa1d03b
FredNekrasov <frednekrasov49@gmail.com>
2025-01-19 12:46:31 +0300

    refactoring the AidlServiceConnector.connect by calling the bindService method and if the result is true, then waiting for the onServiceConnected method to finish, otherwise ending with the return of data

--- aidlsdk/src/main/java/com/fredprojects/aidlsdk/AidlServiceConnector.kt
+++ aidlsdk/src/main/java/com/fredprojects/aidlsdk/AidlServiceConnector.kt
@@ -3,30 +3,29 @@ package com.fredprojects.aidlsdk
 import android.content.*
 import android.os.IBinder
 import android.util.Log
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 internal class AidlServiceConnector(
     private val context: Context
 ) {
-    var userStorage: UserAIDLWithCallback? = null
-        private set
-    private val serviceConnection by lazy {
-        object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                Log.e("fred", "onServiceConnected ${service == null}")
-                userStorage = UserAIDLWithCallback.Stub.asInterface(service)
-            }
-            override fun onServiceDisconnected(name: ComponentName?) {
-                Log.e("fred", "onServiceDisconnected")
-                userStorage = null
-            }
-        }
-    }
-    fun connect() {
+    suspend fun connect(): Pair<ServiceConnection, UserAIDLWithCallback?> = suspendCancellableCoroutine {
         Log.e("fred", "connect")
-        context.bindService(Intent(AIDL_SERVICE_ACTION).setPackage(AIDL_SERVICE_PACKAGE), serviceConnection, Context.BIND_AUTO_CREATE)
+        val serviceConnection = object : ServiceConnection {
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                it.resume(this to UserAIDLWithCallback.Stub.asInterface(service))
+            }
+            override fun onServiceDisconnected(name: ComponentName?) {}
+        }
+        val isConnected = context.bindService(
+            Intent(AIDL_SERVICE_ACTION).setPackage(AIDL_SERVICE_PACKAGE),
+            serviceConnection,
+            Context.BIND_AUTO_CREATE
+        )
+        if(!isConnected) it.resume(serviceConnection to null)
     }
-    fun disconnect() {
-        Log.e("fred", "disconnect")
+    fun disconnect(serviceConnection: ServiceConnection) {
+        Log.d("fred", "disconnect")
         context.unbindService(serviceConnection)
     }
     companion object {
