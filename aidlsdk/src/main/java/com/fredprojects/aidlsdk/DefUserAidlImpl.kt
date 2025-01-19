34cdcb5647cbc4f516b41ce01e3ebac4b9a05838
FredNekrasov <frednekrasov49@gmail.com>
2025-01-19 12:52:01 +0300

    refactoring the DefUserAidlImpl.getUserInfoOrNull, setUserInfo functions and creating a new method DefUserAidlImpl.awaitResponse that perform action then wait the result from the callback

--- aidlsdk/src/main/java/com/fredprojects/aidlsdk/DefUserAidlImpl.kt
+++ aidlsdk/src/main/java/com/fredprojects/aidlsdk/DefUserAidlImpl.kt
@@ -4,55 +4,51 @@ import android.content.Context
 import android.util.Log
 import com.fredprojects.aidlsdk.models.UserInfo
 import com.fredprojects.aidlsdk.utils.ResultCallback
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.suspendCancellableCoroutine
+import kotlin.coroutines.resume
 
 class DefUserAidlImpl(
     private val context: Context
 ) {
-    private var userData: UserInfo? = null
     private val serviceConnector by lazy { AidlServiceConnector(context) }
-    private val defResultCallback by lazy {
-        object : ResultCallback.Stub() {
+    private suspend fun awaitResponse(
+        action: (ResultCallback) -> Unit
+    ): UserInfo? = suspendCancellableCoroutine {
+        val resultCallback = object : ResultCallback.Stub() {
             override fun onSuccess(info: UserInfo?) {
-                Log.e("fred", "ResultCallback onSuccess $info")
-                userData = info
+                Log.e("fred", "ResCallback onSuccess $info")
+                it.resume(info)
             }
             override fun onError(errorMessage: String?) {
-                Log.e("fred", errorMessage.toString())
+                Log.e("fred", "ResCallback onError $errorMessage")
+                it.resume(null)
             }
         }
+        action(resultCallback)
     }
     suspend fun getUserInfoOrNull(): UserInfo? {
-        Log.e("fred", "getUserInfoOrNull start")
-        try {
-            executeFunc { serviceConnector.userStorage?.getUserInfo(defResultCallback) }
+        return try {
+            val (serviceConnection, userStorage) = serviceConnector.connect()
+            if(userStorage == null) return null
+            val user = awaitResponse { userStorage.getUserInfo(it) }
+            serviceConnector.disconnect(serviceConnection)
+            user
         } catch(e: Exception) {
-            return null
+            null
         }
-        Log.e("fred", "getUserInfoOrNull end")
-        return userData
     }
     /**
      * @return true if success otherwise false
      */
-    suspend fun setUserInfo(userInfo: UserInfo): Boolean = with(serviceConnector) {
-        Log.e("fred", "setUserInfo start")
-        if(userStorage == null) return false
-        try {
-            executeFunc { userStorage?.setUserInfo(userInfo, defResultCallback) }
+    suspend fun setUserInfo(userInfo: UserInfo): Boolean {
+        return try {
+            val (serviceConnection, userStorage) = serviceConnector.connect()
+            if(userStorage == null) return false
+            val user = awaitResponse { userStorage.setUserInfo(userInfo, it) }
+            serviceConnector.disconnect(serviceConnection)
+            user != null
         } catch(e: Exception) {
-            return false
+            false
         }
-        Log.e("fred", "setUserInfo end")
-        return true
-    }
-    private suspend fun executeFunc(action: () -> Unit) = with(serviceConnector) {
-        connect()
-        withContext(Dispatchers.IO) {
-            action()
-            Log.e("fred", "$userStorage userStorage, $userData userData")
-        }
-        disconnect()
     }
 }
