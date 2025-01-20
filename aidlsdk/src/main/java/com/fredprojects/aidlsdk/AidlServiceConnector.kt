694653c100100ab21f96f8ed3ee0d5021a95d7f5
FredNekrasov <frednekrasov49@gmail.com>
2025-01-20 15:26:53 +0300

    refactoring the AidlServiceConnector. Separating the creation of serviceConnection and the use of bindService from the connect function to functions

--- aidlsdk/src/main/java/com/fredprojects/aidlsdk/AidlServiceConnector.kt
+++ aidlsdk/src/main/java/com/fredprojects/aidlsdk/AidlServiceConnector.kt
@@ -2,25 +2,35 @@ package com.fredprojects.aidlsdk
 
 import android.content.*
 import android.os.IBinder
+import kotlinx.coroutines.CancellableContinuation
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
 internal class AidlServiceConnector(
     private val context: Context
 ) {
-    suspend fun connect(): Pair<ServiceConnection, UserAIDLWithCallback?> = suspendCancellableCoroutine {
-        val serviceConnection = object : ServiceConnection {
-            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-                it.resume(this to UserAIDLWithCallback.Stub.asInterface(service))
-            }
-            override fun onServiceDisconnected(name: ComponentName?) {}
+    private fun createServiceConnection(
+        continuation: CancellableContinuation<Pair<ServiceConnection, UserAIDLWithCallback?>>
+    ): ServiceConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            continuation.resume(this to UserAIDLWithCallback.Stub.asInterface(service))
         }
-        val isConnected = context.bindService(
-            Intent(AIDL_SERVICE_ACTION).setPackage(AIDL_SERVICE_PACKAGE),
-            serviceConnection,
-            Context.BIND_AUTO_CREATE
-        )
+        override fun onServiceDisconnected(name: ComponentName?) {
+            continuation.resume(this to null)
+        }
+    }
+    private fun bindService(serviceConnection: ServiceConnection): Boolean = context.bindService(
+        Intent(AIDL_SERVICE_ACTION).setPackage(AIDL_SERVICE_PACKAGE),
+        serviceConnection,
+        Context.BIND_AUTO_CREATE
+    )
+    suspend fun connect(): Pair<ServiceConnection, UserAIDLWithCallback?> = suspendCancellableCoroutine {
+        val serviceConnection = createServiceConnection(it)
+        val isConnected = bindService(serviceConnection)
         if(!isConnected) it.resume(serviceConnection to null)
+        it.invokeOnCancellation { _ ->
+            if(isConnected) disconnect(serviceConnection)
+        }
     }
     fun disconnect(serviceConnection: ServiceConnection) {
         context.unbindService(serviceConnection)
