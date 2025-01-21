4c413c67b8056c5b938f21f96c54902542a91546
FredNekrasov <frednekrasov49@gmail.com>
2025-01-21 17:51:25 +0300

    adding fake exceptions for testing and logging. refactoring the DefUserAidlImpl.executeUserStorageAction after testing

--- aidlsdk/src/main/java/com/fredprojects/aidlsdk/DefUserAidlImpl.kt
+++ aidlsdk/src/main/java/com/fredprojects/aidlsdk/DefUserAidlImpl.kt
@@ -1,6 +1,7 @@
 package com.fredprojects.aidlsdk
 
 import android.content.Context
+import android.util.Log
 import com.fredprojects.aidlsdk.models.UserInfo
 import com.fredprojects.aidlsdk.utils.ResultCallback
 import kotlinx.coroutines.*
@@ -10,6 +11,7 @@ class DefUserAidlImpl(
     private val context: Context
 ) {
     private val serviceConnector by lazy { AidlServiceConnector(context) }
+    private var i = 0
     private suspend fun awaitResponse(
         action: (ResultCallback) -> Unit
     ): UserInfo? = suspendCancellableCoroutine { continuation ->
@@ -18,32 +20,50 @@ class DefUserAidlImpl(
                 continuation.resume(info)
             }
             override fun onError(errorMessage: String?) {
+                Log.e("fred", "onError: $errorMessage")
                 continuation.resume(null)
             }
         }
         action(resultCallback)
+        if(i % 3 == 2) {
+            i++
+            continuation.cancel(Exception("error awaitResponse"))
+        }
         continuation.invokeOnCancellation {
             resultCallback.onError(it?.message)
         }
     }
     suspend fun getUserInfoOrNull(): UserInfo? = executeUserStorageAction { userStorage ->
-        awaitResponse { userStorage.getUserInfo(it) }
+        awaitResponse {
+            if(i % 3 == 1) {
+                i++
+                throw Exception("error getUserInfoOrNull")
+            }
+            userStorage.getUserInfo(it)
+        }
     }
     /**
      * @return true if success otherwise false
      */
     suspend fun setUserInfo(userInfo: UserInfo): Boolean = executeUserStorageAction { userStorage ->
-        awaitResponse { userStorage.setUserInfo(userInfo, it) }
+        awaitResponse {
+            if(i % 3 == 0) {
+                i++
+                throw Exception("error setUserInfo")
+            }
+            userStorage.setUserInfo(userInfo, it)
+        }
     } != null
     private suspend fun<T> executeUserStorageAction(action: suspend (UserAIDLWithCallback) -> T): T? {
-        return try {
-            val (serviceConnection, userStorage) = serviceConnector.connect()
-            if(userStorage == null) return null
-            action(userStorage).also {
-                serviceConnector.disconnect(serviceConnection)
-            }
-        } catch (e: Exception) {
-            null
-        }
+        val (serviceConnection, userStorage) = serviceConnector.connect()
+        if(userStorage == null) return null
+        return runCatching {
+            action(userStorage)
+        }.onSuccess {
+            serviceConnector.disconnect(serviceConnection)
+        }.onFailure { e ->
+            Log.w("fred", "$i executeUserStorageAction: ${e.message}")
+            serviceConnector.disconnect(serviceConnection)
+        }.getOrNull()
     }
 }
--- aidlservice/src/main/java/com/fredprojects/aidlservice/UserServiceWithCallbacks.kt
+++ aidlservice/src/main/java/com/fredprojects/aidlservice/UserServiceWithCallbacks.kt
@@ -3,7 +3,6 @@ package com.fredprojects.aidlservice
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import android.os.RemoteException
 import android.util.Log
 import com.fredprojects.aidlsdk.UserAIDLWithCallback
 import com.fredprojects.aidlsdk.models.UserInfo
@@ -11,31 +10,35 @@ import com.fredprojects.aidlsdk.utils.ResultCallback
 import kotlinx.coroutines.*
 
 class UserServiceWithCallbacks : Service() {
-    private val scope by lazy { CoroutineScope(Dispatchers.IO) }
+    private val scope by lazy { CoroutineScope(Dispatchers.Default) }
     private var userData: UserInfo? = null
     override fun onBind(intent: Intent): IBinder {
         return object : UserAIDLWithCallback.Stub() {
             override fun getUserInfo(callback: ResultCallback?) {
                 if(callback == null) return println("fred: callback is null")
                 try {
+                    if(userData == null) throw Exception("userData is null")
                     scope.launch {
                         delay(3000)
                         Log.e("fred", userData.toString())
                         callback.onSuccess(userData)
                     }
-                } catch (e: RemoteException) {
+                } catch (e: Exception) {
+                    Log.e("fred", "UserServiceWithCallbacks: ${e.message}")
                     callback.onError(e.message)
                 }
             }
             override fun setUserInfo(userInfo: UserInfo?, callback: ResultCallback?) {
                 if(callback == null) return println("fred: callback is null")
                 try {
+                    if(userInfo?.login == "null") throw Exception("login is null")
                     scope.launch {
                         delay(3000)
                         userData = userInfo
-                        callback.onSuccess(userInfo)
+                        callback.onSuccess(userData)
                     }
-                } catch(e: RemoteException) {
+                } catch(e: Exception) {
+                    Log.e("fred", "UserServiceWithCallbacks: ${e.message}")
                     callback.onError(e.message)
                 }
             }
